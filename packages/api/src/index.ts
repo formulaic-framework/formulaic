45ac1aac56d9cdb99f7121f36244f0ea5c8eb482
Flyyn <_@rst.vn>
2022-07-08 11:44:53 -0400

    Package auth-module with api

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,4 +1,22 @@
 export {
+  Acl,
+  AuthGuard,
+  AuthModule,
+  AuthModuleAsyncOptions,
+  AuthModuleOptions,
+  CurrentJWT,
+  CurrentUser,
+  CurrentUserFetch,
+  CurrentUserId,
+  MaybePromise,
+  ParseUser,
+  Private,
+  Public,
+  RequireOneOfRole,
+  RequireRoles,
+} from "@formulaic/auth-module";
+
+export {
   IDConfig,
   IDDefinition,
   IDModule,
